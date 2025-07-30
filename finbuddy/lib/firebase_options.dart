1fa98fe0381cbfb8a9e98ed5f1b97e5b7b0f25d2
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-07-30 20:35:28 -0300

    adicionado tela de login

--- finbuddy/lib/firebase_options.dart
+++ finbuddy/lib/firebase_options.dart
@@ -1,22 +1,25 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, TargetPlatform;
-
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return macos;
+      case TargetPlatform.windows:
+        return windows;
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -24,11 +27,51 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDGxYfNSkDIY8tABUMYMSobgCMMvrhx8gs',
+    appId: '1:752699470468:web:a1b462b7dbc4c80b2988d1',
+    messagingSenderId: '752699470468',
+    projectId: 'finbuddy-b0763',
+    authDomain: 'finbuddy-b0763.firebaseapp.com',
+    storageBucket: 'finbuddy-b0763.firebasestorage.app',
+    measurementId: 'G-WVKGEW3K1Y',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyD2HVpv-HmBfJ4iaVvJg82gym9dO8fEiaU',
-    appId: '1:543019960746:android:fcb1c41ada80dff25b43d3',
-    messagingSenderId: '543019960746',
-    projectId: 'daletrasufape',
-    storageBucket: 'daletrasufape.appspot.com',
+    apiKey: 'AIzaSyAmdGEqAsBjZaNIAT32o4xjeTApcVgNtzg',
+    appId: '1:752699470468:android:3257be7bd14be66e2988d1',
+    messagingSenderId: '752699470468',
+    projectId: 'finbuddy-b0763',
+    storageBucket: 'finbuddy-b0763.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyC5OrQuKehClVsJ-UYUTpFMSGExs0YtY60',
+    appId: '1:752699470468:ios:991498026191fc0c2988d1',
+    messagingSenderId: '752699470468',
+    projectId: 'finbuddy-b0763',
+    storageBucket: 'finbuddy-b0763.firebasestorage.app',
+    iosClientId: '752699470468-m7gdq6m8bt1g9ca4jlmp3c7b11nn04s0.apps.googleusercontent.com',
+    iosBundleId: 'com.example.finbuddy',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyC5OrQuKehClVsJ-UYUTpFMSGExs0YtY60',
+    appId: '1:752699470468:ios:991498026191fc0c2988d1',
+    messagingSenderId: '752699470468',
+    projectId: 'finbuddy-b0763',
+    storageBucket: 'finbuddy-b0763.firebasestorage.app',
+    iosClientId: '752699470468-m7gdq6m8bt1g9ca4jlmp3c7b11nn04s0.apps.googleusercontent.com',
+    iosBundleId: 'com.example.finbuddy',
+  );
+
+  static const FirebaseOptions windows = FirebaseOptions(
+    apiKey: 'AIzaSyDGxYfNSkDIY8tABUMYMSobgCMMvrhx8gs',
+    appId: '1:752699470468:web:eacd15043c878c9e2988d1',
+    messagingSenderId: '752699470468',
+    projectId: 'finbuddy-b0763',
+    authDomain: 'finbuddy-b0763.firebaseapp.com',
+    storageBucket: 'finbuddy-b0763.firebasestorage.app',
+    measurementId: 'G-YV30Z7P3ER',
   );
 }
