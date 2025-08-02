6c10410c0c2e074e43222b6746cd6cd05be21ff3
Flayban <davidflayban2@hotmail.com>
2025-08-02 14:38:03 -0300

    Correcao de versoes do SDK, compiler, NDK

--- finbuddy/android/settings.gradle.kts
+++ finbuddy/android/settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
         flutterSdkPath
     }
 
-    // IMPORTANTE: inclui o build do Flutter Tools para encontrar o plugin flutter-plugin-loader
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
@@ -20,7 +19,9 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
