e3c1ceb12d24d8ddc88361c5f98fcc217c1834d9
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-08-01 21:39:21 -0300

    splash e tela de gastos adicionada

---
+++ finbuddy/lib/screens/splash/splash_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:finbuddy/components/utils/vertical_spacer_box.dart';
+import 'package:finbuddy/screens/screens_index.dart';
+import 'package:finbuddy/screens/splash/splash_screen_controller.dart';
+import 'package:finbuddy/shared/constants/app_enums.dart';
+import 'package:finbuddy/shared/constants/app_number_constants.dart';
+import 'package:finbuddy/shared/constants/style_constants.dart';
+import 'package:finbuddy/shared/core/navigator.dart';
+import '../../shared/core/assets_index.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late final SplashScreenController _controller;
+  late final AnimationController animController;
+  double opacity = 0;
+  @override
+  void initState() {
+    super.initState();
+    animController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _controller = SplashScreenController(context);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setController();
+      stopController();
+      _controller.initApplication(() {});
+    });
+  }
+
+  void setController() async {
+    await animController.repeat();
+  }
+
+  void stopController() async {
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        opacity = 1;
+      });
+      animController.stop();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   actions: [
+      //     IconButton(
+      //         onPressed: () async {
+      //           animController.repeat();
+      //           stopController();
+      //         },
+      //         icon: Icon(Icons.add))
+      //   ],
+      // ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AnimatedOpacity(duration: const Duration(milliseconds: 200), opacity: opacity, child: const Text('FinBuddy')),
+                const VerticalSpacerBox(size: SpacerSize.huge)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'From DevSphere',
+                style: kCaption2.copyWith(fontFamily: 'Roboto'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ finbuddy/lib/screens/splash/splash_screen_controller.dart
@@ -0,0 +1,62 @@
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logging/logging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:finbuddy/screens/screens_index.dart';
+import 'package:finbuddy/shared/core/navigator.dart';
+import 'package:finbuddy/shared/core/preferences_manager.dart';
+import 'package:finbuddy/shared/core/user_manager.dart';
+import 'package:finbuddy/shared/core/user_storage.dart';
+
+class SplashScreenController {
+  final BuildContext context;
+  bool isFirstTime = false;
+  SplashScreenController(this.context);
+  final Logger _logger = Logger('Splash screen logger');
+  final userStorage = UserStorage();
+
+  void initApplication(Function onComplete) async {
+    await Future.delayed(const Duration(seconds: 3), () async {
+      await configDefaultAppSettings();
+    });
+  }
+
+  Future configDefaultAppSettings() async {
+    _logger.config('Configuring default app settings...');
+    const String loadedKey = 'loadedFirstTime';
+    final prefs = await SharedPreferences.getInstance();
+    PreferencesManager.saveIsFirstTime();
+    setupGetIt();
+
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    _logger.fine('Default app settings configured!');
+    final bool? isFirstTime = prefs.getBool(loadedKey);
+    if (isFirstTime != null && isFirstTime) {
+      log('First time user in: carrosel');
+      // navigatorKey.currentState!.pushNamed(Screens.carrousel);
+    } else {
+      log('User already open app: sign in or home');
+      FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        log('Alright, checking firebase auth user');
+        if (user == null) {
+          log('User is $user');
+          navigatorKey.currentState!.pushReplacementNamed(Screens.signin);
+        } else {
+          navigatorKey.currentState!.pushReplacementNamed(Screens.home);
+        }
+      });
+      return;
+    }
+  }
+
+  void setupGetIt() {
+    final getIt = GetIt.instance;
+    getIt.registerSingleton<UserManager>(UserManager());
+  }
+}
