be756ba4b6e73e47717e9a75e25459eca0391b15
JoseDanielF <danielduartefilho.df@gmail.com>
2025-08-11 18:55:30 -0300

    ajustado rotas

--- finbuddy/lib/screens/Home/home_screen.dart
+++ finbuddy/lib/screens/Home/home_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import '../Perfil/profile_screen.dart';
-// import 'calendar_screen.dart';
+import '../Profile/profile_screen.dart';
+import '../Calendar/calendar_screen.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 FlutterLocalNotificationsPlugin();
