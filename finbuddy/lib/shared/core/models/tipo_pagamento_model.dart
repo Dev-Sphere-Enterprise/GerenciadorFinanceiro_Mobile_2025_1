72fddc2995d018f5dc718f3b573bbe37c97f3227
Flayban <davidflayban2@hotmail.com>
2025-09-01 19:09:57 -0300

    feito correcoes no tipo_pagamento_model no metodo fromMap

--- finbuddy/lib/shared/core/models/tipo_pagamento_model.dart
+++ finbuddy/lib/shared/core/models/tipo_pagamento_model.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class TipoPagamentoModel {
   final String? id;
   final String nome;
+  final bool usaCartao;
+  final bool parcelavel;
   final bool deletado;
   final DateTime dataCriacao;
   final DateTime dataAtualizacao;
@@ -10,6 +12,8 @@ class TipoPagamentoModel {
   TipoPagamentoModel({
     this.id,
     required this.nome,
+    this.usaCartao = false,
+    this.parcelavel = false,
     this.deletado = false,
     required this.dataCriacao,
     required this.dataAtualizacao,
@@ -18,6 +22,8 @@ class TipoPagamentoModel {
   Map<String, dynamic> toMap() {
     return {
       'Nome': nome,
+      'Usa_Cartao': usaCartao,
+      'Parcelavel': parcelavel,
       'Deletado': deletado,
       'Data_Criacao': Timestamp.fromDate(dataCriacao),
       'Data_Atualizacao': Timestamp.fromDate(dataAtualizacao),
@@ -27,10 +33,32 @@ class TipoPagamentoModel {
   factory TipoPagamentoModel.fromMap(String id, Map<String, dynamic> map) {
     return TipoPagamentoModel(
       id: id,
-      nome: map['Nome'] ?? '',
-      deletado: map['Deletado'] ?? false,
-      dataCriacao: (map['Data_Criacao'] as Timestamp).toDate(),
-      dataAtualizacao: (map['Data_Atualizacao'] as Timestamp).toDate(),
+      nome: map['Nome'] as String? ?? '',
+      usaCartao: map['Usa_Cartao'] as bool? ?? false,
+      parcelavel: map['Parcelavel'] as bool? ?? false,
+      deletado: map['Deletado'] as bool? ?? false,
+      dataCriacao: (map['Data_Criacao'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      dataAtualizacao: (map['Data_Atualizacao'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+
+  TipoPagamentoModel copyWith({
+    String? id,
+    String? nome,
+    bool? usaCartao,
+    bool? parcelavel,
+    bool? deletado,
+    DateTime? dataCriacao,
+    DateTime? dataAtualizacao,
+  }) {
+    return TipoPagamentoModel(
+      id: id ?? this.id,
+      nome: nome ?? this.nome,
+      usaCartao: usaCartao ?? this.usaCartao,
+      parcelavel: parcelavel ?? this.parcelavel,
+      deletado: deletado ?? this.deletado,
+      dataCriacao: dataCriacao ?? this.dataCriacao,
+      dataAtualizacao: dataAtualizacao ?? this.dataAtualizacao,
     );
   }
 }
