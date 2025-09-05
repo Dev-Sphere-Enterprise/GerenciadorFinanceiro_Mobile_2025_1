7a9e7af03be5a3ea611165b1b3c6ac392d72a27b
JoseDanielF <danielduartefilho.df@gmail.com>
2025-09-05 20:41:12 -0300

    refatorado tela de tipos de pagamento

--- finbuddy/lib/screens/TiposPagamentos/viewmodel/tipos_pagamento_viewmodel.dart
+++ finbuddy/lib/screens/TiposPagamentos/viewmodel/tipos_pagamento_viewmodel.dart
@@ -1,26 +1,41 @@
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import '../../../shared/core/models/tipo_pagamento_model.dart';
 import '../../../shared/core/repositories/tipos_pagamento_repository.dart';
 
 class TiposPagamentoViewModel extends ChangeNotifier {
-  final TipoPagamentoRepository _repository = TipoPagamentoRepository();
+  final TipoPagamentoRepository _repository;
 
-  late Stream<List<TipoPagamentoModel>> tiposUsuarioStream;
   List<TipoPagamentoModel> _tiposGerais = [];
-  List<TipoPagamentoModel> get tiposGerais => _tiposGerais;
+  List<TipoPagamentoModel> _tiposUsuario = [];
 
-  TiposPagamentoViewModel() {
-    tiposUsuarioStream = _repository.getTiposStream();
-    _loadTiposGerais();
+  final _combinedStreamController = StreamController<List<TipoPagamentoModel>>.broadcast();
+  StreamSubscription? _tiposUsuarioSubscription;
+
+  Stream<List<TipoPagamentoModel>> get tiposCombinadosStream => _combinedStreamController.stream;
+
+  TiposPagamentoViewModel({TipoPagamentoRepository? repository})
+      : _repository = repository ?? TipoPagamentoRepository() {
+    _tiposUsuarioSubscription = _repository.getTiposStream().listen((tiposUsuario) {
+      _tiposUsuario = tiposUsuario;
+      _combineAndEmit();
+    });
   }
 
-  Future<void> _loadTiposGerais() async {
+  Future<void> loadTiposGerais() async {
+    if (_tiposGerais.isNotEmpty) return;
     try {
       _tiposGerais = await _repository.getTiposGerais();
-      notifyListeners();
+      _combineAndEmit();
     } catch (e) {
       debugPrint("Erro ao carregar tipos gerais: $e");
+      _combinedStreamController.addError(e);
     }
+  }
+
+  void _combineAndEmit() {
+    final List<TipoPagamentoModel> tiposCombinados = [..._tiposGerais, ..._tiposUsuario];
+    _combinedStreamController.add(tiposCombinados);
   }
 
   Future<void> excluirTipo(String tipoId) async {
@@ -30,11 +45,17 @@ class TiposPagamentoViewModel extends ChangeNotifier {
   Future<bool> salvarTipo(TipoPagamentoModel tipo) async {
     try {
       await _repository.addOrEditTipo(tipo);
-      notifyListeners();
       return true;
     } catch (e) {
       debugPrint("Erro ao salvar tipo de pagamento: $e");
       return false;
     }
+  }
+
+  @override
+  void dispose() {
+    _tiposUsuarioSubscription?.cancel();
+    _combinedStreamController.close();
+    super.dispose();
   }
 }
